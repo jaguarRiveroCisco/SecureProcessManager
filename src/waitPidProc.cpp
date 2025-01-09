1b820ca23ea99b6c1d1401dea8daed0068014309
Jaguar Rivero <jarivero@cisco.com>
2025-01-09 15:00:26 -0800

    Refactor argument parsing in main function; improve error handling and default values for process configuration; enhance readability by separating logic into parseArguments function

--- src/waitPidProc.cpp
+++ src/waitPidProc.cpp
@@ -11,18 +11,14 @@
 std::atomic<bool> g_display = true;
 std::atomic<bool> g_running = true;
 
-auto main(int argc, char *argv[]) -> int
+void parseArguments(int argc, char *argv[], int &numProcesses, std::string &processType, int &rndUpper,
+                    std::atomic<bool> &g_display)
 {
-    int         numProcesses = 4;
-    std::string processType  = "simul";
-    int         rndUpper     = 10; // Default value for rndUpper
-
-    auto parseArguments = [&]() {
-        int opt;
-        while ((opt = getopt(argc, argv, "n:t:r:d:h")) != -1)
+    int opt;
+    while ((opt = getopt(argc, argv, "n:t:r:d:h")) != -1)
+    {
+        switch (opt)
         {
-            switch (opt)
-            {
             case 'n':
                 // Set the number of processes from the argument
                 numProcesses = std::atoi(optarg);
@@ -42,33 +38,41 @@ auto main(int argc, char *argv[]) -> int
             case 'h':
             default:
                 // Display usage information and exit
-                    std::cerr << "Usage: " << argv[0]
-                              << " -n <number of processes> -t <process type 'real' or 'simul' (default)> -r <random upper limit> -d <display (0 "
-                                 "or 1)> -h -> help\n";
-                    std::exit(EXIT_SUCCESS);
-            }
+                std::cerr << "Usage: " << argv[0]
+                          << " -n <number of processes> -t <process type 'real' or 'simul' (default)> -r <random upper "
+                             "limit> -d <display (0 or 1)> -h -> help\n";
+                std::exit(EXIT_SUCCESS);
         }
+    }
 
-        if (numProcesses <= 0)
-        {
-            std::cerr << "Number of processes must be greater than 0. Defaulting to 4.\n";
-            numProcesses = 4;
-        }
+    if (numProcesses <= 0)
+    {
+        std::cerr << "Number of processes must be greater than 0. Defaulting to 4.\n";
+        numProcesses = 4;
+    }
 
-        if (processType != "real" && processType != "simul")
-        {
-            std::cerr << "Invalid process type: " << processType << ". Defaulting to 'simul'.\n";
-            processType = "simul";
-        }
+    if (processType != "real" && processType != "simul")
+    {
+        std::cerr << "Invalid process type: " << processType << ". Defaulting to 'simul'.\n";
+        processType = "simul";
+    }
 
-        if (rndUpper < 10)
-        {
-            std::cerr << "Random upper limit must be greater than 10. Defaulting to 10.\n";
-            rndUpper = 10;
-        }
+    if (rndUpper < 10)
+    {
+        std::cerr << "Random upper limit must be greater than 10. Defaulting to 10.\n";
+        rndUpper = 10;
+    }
+}
 
-    };
-    parseArguments();
+auto main(int argc, char *argv[]) -> int
+{
+    int         numProcesses = 4;
+    std::string processType  = "simul";
+    int         rndUpper     = 10; // Default value for rndUpper
+
+
+
+    parseArguments(argc, argv, numProcesses, processType, rndUpper, g_display);
 
     process::controller::printHelp(); // Call to printHelp
 
