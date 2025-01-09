2f2c8f8487d2bed1db64bb723466fc1b4d406f03
Jaguar Rivero <jarivero@cisco.com>
2025-01-09 10:15:59 -0800

    Add process control functionality with command handling for process management

---
+++ src/process_control.cpp
@@ -0,0 +1,102 @@
+#include <atomic>
+#include <iostream>
+#include <string>
+#include <thread>
+#include "process_handler.h"
+
+extern std::atomic<bool> g_display;
+extern std::atomic<bool> g_running;
+
+namespace process
+{
+    void processControl()
+    {
+        std::string input;
+        while (g_running)
+        {
+            std::getline(std::cin, input);
+            if (!input.empty())
+            {
+                if (input == "print on")
+                {
+                    g_display = true;
+                    std::cout << "Display progress turned on." << std::endl;
+                }
+                else if (input == "print off")
+                {
+                    g_display = false;
+                    std::cout << "Display progress turned off." << std::endl;
+                }
+                else if (input == "exit")
+                {
+                    g_running = false; // Set running to false to signal the main thread
+                    std::cout << "Exiting program once all processes are done" << std::endl;
+                }
+                else if (input == "terminate all")
+                {
+                    g_running = false; // Set running to false to signal the main thread
+                    process::Controller::terminateAll();
+                }
+                else if (input.rfind("terminate ", 0) == 0) // Check if input starts with "terminate "
+                {
+                    try
+                    {
+                        pid_t pid = std::stoi(input.substr(10)); // Extract PID from input
+                        process::Controller::terminateProcessByPid(pid);
+                    }
+                    catch (const std::invalid_argument &)
+                    {
+                        std::cerr << "Invalid PID format." << std::endl;
+                    }
+                    catch (const std::out_of_range &)
+                    {
+                        std::cerr << "PID out of range." << std::endl;
+                    }
+                }
+                else if (input == "kill all")
+                {
+                    g_running = false; // Set running to false to signal the main thread
+                    process::Controller::killAll();
+                }
+                else if (input.rfind("kill ", 0) == 0) // Check if input starts with "kill "
+                {
+                    try
+                    {
+                        pid_t pid = std::stoi(input.substr(5)); // Extract PID from input
+                        process::Controller::killProcessByPid(pid);
+                    }
+                    catch (const std::invalid_argument &)
+                    {
+                        std::cerr << "Invalid PID format." << std::endl;
+                    }
+                    catch (const std::out_of_range &)
+                    {
+                        std::cerr << "PID out of range." << std::endl;
+                    }
+                }
+                else if (input == "display pids")
+                {
+                    process::Controller::displayAllPids();
+                }
+                else if (input == "help")
+                {
+                    std::cout << "Available commands:\n"
+                              << "  print on       - Turn on display progress\n"
+                              << "  print off      - Turn off display progress\n"
+                              << "  exit           - Exit the program once all processes are done\n"
+                              << "  terminate all  - Terminate all processes and exit the program\n"
+                              << "  terminate <pid> - Terminate a specific process by PID\n"
+                              << "  kill all       - Kill all processes and exit the program\n"
+                              << "  kill <pid>     - Kill a specific process by PID\n"
+                              << "  display pids   - Display all current PIDs\n"
+                              << "  help           - Display this help message\n";
+                }
+                else
+                {
+                    std::cout << "You entered: " << input << std::endl;
+                }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+}
--- src/waitPidProc.cpp
+++ src/waitPidProc.cpp
@@ -6,100 +6,10 @@
 #include "process.h"
 #include "process_handler.h"
 #include "simul_process.h"
+#include "process_control.h"
 
 std::atomic<bool> g_display = true;
 std::atomic<bool> g_running = true;
-
-void processControl()
-{
-    std::string input;
-    while (g_running)
-    {
-        std::getline(std::cin, input);
-        if (!input.empty())
-        {
-            if (input == "print on")
-            {
-                g_display = true;
-                std::cout << "Display progress turned on." << std::endl;
-            }
-            else if (input == "print off")
-            {
-                g_display = false;
-                std::cout << "Display progress turned off." << std::endl;
-            }
-            else if (input == "exit")
-            {
-                g_running = false; // Set running to false to signal the main thread
-                std::cout << "Exiting program once all processes are done" << std::endl;
-            }
-            else if (input == "terminate all")
-            {
-                g_running = false; // Set running to false to signal the main thread
-                process::Controller::terminateAll();
-            }
-            else if (input.rfind("terminate ", 0) == 0) // Check if input starts with "terminate "
-            {
-                try
-                {
-                    pid_t pid = std::stoi(input.substr(10)); // Extract PID from input
-                    process::Controller::terminateProcessByPid(pid);
-                }
-                catch (const std::invalid_argument &)
-                {
-                    std::cerr << "Invalid PID format." << std::endl;
-                }
-                catch (const std::out_of_range &)
-                {
-                    std::cerr << "PID out of range." << std::endl;
-                }
-            }
-            else if (input == "kill all")
-            {
-                g_running = false; // Set running to false to signal the main thread
-                process::Controller::killAll();
-            }
-            else if (input.rfind("kill ", 0) == 0) // Check if input starts with "kill "
-            {
-                try
-                {
-                    pid_t pid = std::stoi(input.substr(5)); // Extract PID from input
-                    process::Controller::killProcessByPid(pid);
-                }
-                catch (const std::invalid_argument &)
-                {
-                    std::cerr << "Invalid PID format." << std::endl;
-                }
-                catch (const std::out_of_range &)
-                {
-                    std::cerr << "PID out of range." << std::endl;
-                }
-            }
-            else if (input == "display pids")
-            {
-                process::Controller::displayAllPids();
-            }
-            else if (input == "help")
-            {
-                std::cout << "Available commands:\n"
-                          << "  print on       - Turn on display progress\n"
-                          << "  print off      - Turn off display progress\n"
-                          << "  exit           - Exit the program once all processes are done\n"
-                          << "  terminate all  - Terminate all processes and exit the program\n"
-                          << "  terminate <pid> - Terminate a specific process by PID\n"
-                          << "  kill all       - Kill all processes and exit the program\n"
-                          << "  kill <pid>     - Kill a specific process by PID\n"
-                          << "  display pids   - Display all current PIDs\n"
-                          << "  help           - Display this help message\n";
-            }
-            else
-            {
-                std::cout << "You entered: " << input << std::endl;
-            }
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-}
 
 auto main(int argc, char *argv[]) -> int
 {
@@ -158,7 +68,7 @@ auto main(int argc, char *argv[]) -> int
     process::ProcessSimulator::setRndUpper(rndUpper); // Call to setRndUpper with the parsed value
     process::Controller::setProcessType(processType); // Call to setProcessType with the parsed value
 
-    std::thread readerThread(processControl);
+    std::thread readerThread(process::processControl);
 
     // Create process handlers
     process::Controller::createHandlers(numProcesses);
