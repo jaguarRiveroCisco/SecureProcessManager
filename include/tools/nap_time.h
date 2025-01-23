a1b53333bcce4d9ca5d47a8a02525439b0f801be
Jaguar Rivero <jarivero@cisco.com>
2025-01-22 17:10:11 -0800

    Add NapTimeMin enum for extended duration options in nap time settings

--- include/tools/nap_time.h
+++ include/tools/nap_time.h
@@ -7,6 +7,7 @@ namespace tools
 {
     enum NapTimeMs { SMALL = 50, SHORT = 100, MEDIUM = 500, LONG = 1000 };
     enum NapTimeSec { SHORTS = 5, MEDIUMS = 15, LONGS = 30 };
+    enum NapTimeMin { SHORTM = 1, MEDIUMM = 5, LONGM = 10, EXTRA_LONG = 30 };
 }
 
 #endif // TOOLS_NAP_TIME_H
