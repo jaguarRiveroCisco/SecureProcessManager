a0693098c16b1e13c026dfbe8478ca21eb30f15e
Jaguar Rivero <jarivero@cisco.com>
2025-01-29 10:43:37 -0800

    Enhance logging in Communicator for message send/receive operations

--- src/concurrency/communicator.cpp
+++ src/concurrency/communicator.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include "communicator.h"
 #include "logger_instance.h"
+
 namespace concurrency
 {
     void Communicator::sendCreationMessage(const std::string& sleepDuration, pid_t pid)
     {
         std::string messageText = "Child " + std::to_string(pid) +
                                   " created. Sleep duration: " + sleepDuration;
+        std::cout << messageText << std::endl;
+        tools::LoggerManager::getInstance().logInfo("Sending message: " + messageText);
+
         try
         {
             messenger_.sendMessage(messageText, Message::CREATION_MSG);
+            tools::LoggerManager::getInstance().logInfo("Message sent successfully.");
         }
         catch (const std::runtime_error &e)
         {
-            tools::LoggerManager::getInstance() << "Failed to send message: " << e.what();
-            tools::LoggerManager::getInstance().flush(tools::LogLevel::ERROR);
+            tools::LoggerManager::getInstance().logError("Failed to send message: " + std::string(e.what()));
         }
     }
 
-    std::string Communicator::receiveCreationMessage() { return messenger_.receiveMessage(Message::CREATION_MSG); }
+    std::string Communicator::receiveCreationMessage() 
+    { 
+        tools::LoggerManager::getInstance().logInfo("Attempting to receive message.");
+
+        std::string message = messenger_.receiveMessage(Message::CREATION_MSG);
+
+        tools::LoggerManager::getInstance().logInfo("Received message: " + message);
+
+        return message; 
+    }
 } // namespace concurrency
