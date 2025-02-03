b0fdb4af967368064b374be9d1565a11aeda8af6
Jaguar Rivero <jarivero@cisco.com>
2025-02-03 11:01:02 -0800

    removed pre unlink of messenger semaphore.

--- src/ProcessController.cpp
+++ src/ProcessController.cpp
@@ -27,8 +27,7 @@ auto main(int argc, char *argv[]) -> int
     cli::driver::parseArguments(argc, argv, numProcesses, processType);
 
     concurrency::SemaphoreGuard::unlinkSemaphore("/console_logger");
-    concurrency::SemaphoreGuard::unlinkSemaphore("/messenger_logger");
-   
+
     tools::LoggerManager::createLoggerType();
 
     process::MainController::run(processType, numProcesses);
