0f1582e103f9242dd32bf5ad49e3d516eecfadbd
Jaguar Rivero <jarivero@cisco.com>
2025-02-11 12:36:16 -0800

    Enhance argument parsing to include configuration file handling and improve error reporting

--- src/consolecontroller/console_control.cpp
+++ src/consolecontroller/console_control.cpp
@@ -9,95 +9,108 @@ namespace cli::driver
 
     static constexpr int MAX_PROCESSES = 20;
 
-void parseArguments(int argc, char *argv[], int &numProcesses, std::string &processType, tools::ConsoleLogger& cl)
-    {
+    void parseArguments(int argc, char *argv[], int &numProcesses, std::string &processType, tools::ConsoleLogger &cl) {
         int opt;
-        while ((opt = getopt(argc, argv, "n:t:s:l:T:c:h")) != -1) // Added 'c' for config file path
-        {
-            switch (opt)
+        bool configFileProvided = false;
+
+        while ((opt = getopt(argc, argv, "n:t:s:l:T:c:h")) != -1)
+        { // Added 'c' for config file path
+            try
             {
-                case 'n':
-                    // Set the number of processes from the argument
-                    numProcesses = std::strtol(optarg, nullptr, 10);
-                    if (numProcesses <= 0)
-                    {
-                        cl << "Number of processes must be greater than 0. Defaulting to 4.";
-                        cl.flush(tools::LogLevel::WARNING);
-                        numProcesses = 4;
-                    }
-                    numProcesses = std::min(numProcesses, MAX_PROCESSES);
-                    break;
-                case 't':
-                    // Set the process type from the argument
-                    processType = optarg;
-                    if (processType != "real" && processType != "simul" && processType != "network" &&
-                        processType != "system" && processType != "custom")
-                    {
-                        cl << "Valid process types are: real, network, system, simul, custom. Defaulting to simul.";
-                        cl.flush(tools::LogLevel::WARNING);
-                        processType = "simul";
-                    }
-                    break;
-                case 's':
-                    api::respawn(std::strtol(optarg, nullptr, 10) != 0);
-                    break;
-                case 'l':
-                    // Set the logging type from the argument
-                    if (std::string(optarg) == "file")
-                    {
-                        api::loggerType("file");
-                    }
-                    else
-                    {
-                        cl << "Invalid logging type defaulting to " << "console";
-                        cl.flush(tools::LogLevel::WARNING);
-                        api::loggerType("console");
-                    }
-                    break;
-                case 'T':
-                    // Set the nap time type from the argument
-                    if (std::string(optarg) == "MS")
-                    {
-                        api::SleepType(tools::NapType::MS);
-                    }
-                    else if (std::string(optarg) == "SEC")
-                    {
-                        api::SleepType(tools::NapType::SEC);
-                    }
-                    else if (std::string(optarg) == "MIN")
-                    {
-                        api::SleepType(tools::NapType::MIN);
-                    }
-                    else
-                    {
-                        cl << "Invalid nap time type defaulting to " << "MS";
-                        cl.flush(tools::LogLevel::WARNING);
-                        api::SleepType(tools::NapType::MS);
-                    }
-                    break;
-                        case 'c':
+                switch (opt)
+                {
+                    case 'n':
+                        // Set the number of processes from the argument
+                        try
+                        {
+                            numProcesses = std::stoi(optarg);
+                            if (numProcesses <= 0 || numProcesses > MAX_PROCESSES)
+                            {
+                                throw std::out_of_range("Number of processes out of valid range.");
+                            }
+                        }
+                        catch (const std::exception &e)
+                        {
+                            cl << "Invalid number of processes. Defaulting to 4.";
+                            cl.flush(tools::LogLevel::WARNING);
+                            numProcesses = 4;
+                        }
+                        break;
+                    case 't':
+                        // Set the process type from the argument
+                        processType = optarg;
+                        if (processType != "real" && processType != "simul" && processType != "network" &&
+                            processType != "system" && processType != "custom") {
+                            cl << "Valid process types are: real, network, system, simul, custom. Defaulting to simul.";
+                            cl.flush(tools::LogLevel::WARNING);
+                            processType = "simul";
+                        }
+                        break;
+                    case 's':
+                        api::respawn(std::strtol(optarg, nullptr, 10) != 0);
+                        break;
+                    case 'l':
+                        // Set the logging type from the argument
+                        if (std::string(optarg) == "file") {
+                            api::loggerType("file");
+                        } else {
+                            cl << "Invalid logging type defaulting to console";
+                            cl.flush(tools::LogLevel::WARNING);
+                            api::loggerType("console");
+                        }
+                        break;
+                    case 'T':
+                        // Set the nap time type from the argument
+                        if (std::string(optarg) == "MS") {
+                            api::SleepType(tools::NapType::MS);
+                        } else if (std::string(optarg) == "SEC") {
+                            api::SleepType(tools::NapType::SEC);
+                        } else if (std::string(optarg) == "MIN") {
+                            api::SleepType(tools::NapType::MIN);
+                        } else {
+                            cl << "Invalid nap time type defaulting to MS";
+                            cl.flush(tools::LogLevel::WARNING);
+                            api::SleepType(tools::NapType::MS);
+                        }
+                        break;
+                    case 'c':
                         // Set the configuration file path from the argument
                         api::readConfigFile(optarg);
+                        configFileProvided = true;
                         break;
                     case 'h':
                     default:
                         // Display usage information and exit
-                        cl  << argv[0]
-                            << "\n"
-                            << "Usage:\n"
-                            << "-n <number of processes> (max " << MAX_PROCESSES << ")\n"
-                            << "-t <process type 'real', 'network', 'system', 'custom' or 'simul' (default)>\n"
-                            << "-s <respawn (0 or 1)>\n"
-                            << "-l <logging type 'console' or 'file'>\n"
-                            << "-T <nap time type 'MS', 'SEC', 'MIN'>\n"
-                            << "-c <configuration file path>\n"
-                            << "-h help";
+                        cl << argv[0]
+                           << "\n"
+                           << "Usage:\n"
+                           << "-n <number of processes> (max " << MAX_PROCESSES << ")\n"
+                           << "-t <process type 'real', 'network', 'system', 'custom' or 'simul' (default)>\n"
+                           << "-s <respawn (0 or 1)>\n"
+                           << "-l <logging type 'console' or 'file'>\n"
+                           << "-T <nap time type 'MS', 'SEC', 'MIN'>\n"
+                           << "-c <configuration file path>\n"
+                           << "-h help";
                         cl.flush(tools::LogLevel::INFO);
                         std::exit(EXIT_SUCCESS);
+                }
+            }
+            catch (const std::exception &e)
+            {
+                cl << "Error: " << e.what();
+                cl.flush(tools::LogLevel::EXCEPTION);
+                std::exit(EXIT_FAILURE);
             }
         }
-    }
 
+        // Check if processType is 'system' and if the configuration file was provided
+        if (processType == "system" && !configFileProvided)
+        {
+            cl << "Configuration file with parameters (option -c file) must be provided when the process type is 'system'.";
+            cl.flush(tools::LogLevel::ERROR);
+            std::exit(EXIT_FAILURE);
+        }
+    }
     void printContext(const int numProcesses, const std::string &processType)
     {
         // Static variables to store the latest values
