12abd8546ad9bf399606c84052ec7d122861883a
Jaguar Rivero <jarivero@cisco.com>
2025-02-14 10:53:21 -0800

    Refactor compiler setup by encapsulating logic in set_compilers function in set_compilers.cmake

--- cmake/set_compilers.cmake
+++ cmake/set_compilers.cmake
@@ -1,30 +1,28 @@
-# set_compilers.cmake
+function(set_compilers)
+    if(NOT CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+        message(STATUS "Setting compilers.")
+        # Check if the desired compilers exist
+        find_program(GCC_PATH gcc PATHS /usr/bin NO_DEFAULT_PATH)
+        find_program(GXX_PATH g++ PATHS /usr/bin NO_DEFAULT_PATH)
 
-if(NOT CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    message(STATUS "Setting compilers.")
-    # Check if the desired compilers exist
-    find_program(GCC_PATH gcc PATHS /usr/bin NO_DEFAULT_PATH)
-    find_program(GXX_PATH g++ PATHS /usr/bin NO_DEFAULT_PATH)
-
-    # Set the C compiler if not already set correctly
-    if(NOT CMAKE_C_COMPILER STREQUAL "/usr/bin/gcc")
-        if(GCC_PATH)
-            set(CMAKE_C_COMPILER ${GCC_PATH} CACHE STRING "C compiler" FORCE)
-        else()
-            message(FATAL_ERROR "GCC not found at /usr/bin/gcc.")
+        # Set the C compiler if not already set correctly
+        if(NOT CMAKE_C_COMPILER STREQUAL "/usr/bin/gcc")
+            if(GCC_PATH)
+                set(CMAKE_C_COMPILER ${GCC_PATH} CACHE STRING "C compiler" FORCE)
+            else()
+                message(FATAL_ERROR "GCC not found at /usr/bin/gcc.")
+            endif()
         endif()
-    endif()
 
-    # Set the C++ compiler if not already set correctly
-    if(NOT CMAKE_CXX_COMPILER STREQUAL "/usr/bin/g++")
-        if(GXX_PATH)
-            set(CMAKE_CXX_COMPILER ${GXX_PATH} CACHE STRING "C++ compiler" FORCE)
-        else()
-            message(FATAL_ERROR "G++ not found at /usr/bin/g++.")
+        # Set the C++ compiler if not already set correctly
+        if(NOT CMAKE_CXX_COMPILER STREQUAL "/usr/bin/g++")
+            if(GXX_PATH)
+                set(CMAKE_CXX_COMPILER ${GXX_PATH} CACHE STRING "C++ compiler" FORCE)
+            else()
+                message(FATAL_ERROR "G++ not found at /usr/bin/g++.")
+            endif()
         endif()
+    else()
+        message(STATUS "Skipping compiler settings on macOS.")
     endif()
-
-else()
-    message(STATUS "Skipping compiler settings on macOS.")
-endif()
-
+endfunction()
