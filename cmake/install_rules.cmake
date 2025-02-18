19f1450b39af68843699128ec7b68a830699955b
Jaguar Rivero <jarivero@cisco.com>
2025-02-18 11:31:00 -0800

    Organize CMake configuration: separate build types and installation rules into dedicated files

---
+++ cmake/install_rules.cmake
@@ -0,0 +1,16 @@
+# Set default install prefix if not specified
+if(NOT CMAKE_INSTALL_PREFIX)
+    set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE PATH "Install path prefix, prepended onto install directories." FORCE)
+endif()
+
+message(STATUS "Install Prefix: ${CMAKE_INSTALL_PREFIX}")
+
+# Installation rules
+install(TARGETS ProcessController ProcessControllerLib
+        RUNTIME DESTINATION bin
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib
+        )
+
+# Optionally install header files
+install(DIRECTORY include/ DESTINATION include)
